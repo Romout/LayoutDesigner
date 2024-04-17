d916ada27228439716a80000e31bea6d7ef93f75
Ramin Dalkouhi <ramin.dalkouhi@avl.com>
2024-04-17 19:12:06 +0200

    Much cleaner approach

---
+++ Models/LayoutControlData.cs
@@ -0,0 +1,14 @@
+﻿using LayoutDesigner.Pages;
+
+namespace LayoutDesigner.Models
+{
+	public class LayoutControlData
+	{
+		public Type? Type { get; set; }
+		public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+		public bool Selectable { get; set; } = true;
+		public bool IsContainer { get; set; } = false;
+		public List<LayoutControlData> Children { get; set; } = new List<LayoutControlData>();
+		public DropArea? DragSource { get; set; }
+	}
+}
